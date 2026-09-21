9affbab8d1f9952b0b2e5c9b333ded6679eeb7fa
Ahmad Syaifuddin <ahmadsyai598@gmail.com>
2026-09-21 13:25:51 +0800

    ok

--- resources/views/public_register.blade.php
+++ resources/views/public_register.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pendaftaran Anggota Baru - KUD GM</title>
+    <title>Pendaftaran Anggota Baru - KUD GAMA</title>
     
     <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
     
